5c86e2d9cdf6818dc03dad79d78ef11a0c921c7b
Rival <rival.abdrakhmanov@outlook.com>
2020-06-08 21:23:56 +0300

    Move to logic Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Hosting;
 
 namespace SimpleAspNetCoreProject
@@ -7,14 +9,23 @@ namespace SimpleAspNetCoreProject
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
-                });
+                    webBuilder.Configure(builder =>
+                    {
+                        builder.UseRouting();
+                        builder.UseEndpoints(endpoints =>
+                        {
+                            endpoints.MapGet("/", async context =>
+                            {
+                                await context.Response.WriteAsync("Hello World!");
+                            });
+                        });
+                    });
+                })
+                .Build()
+                .Run();
+        }
     }
 }
